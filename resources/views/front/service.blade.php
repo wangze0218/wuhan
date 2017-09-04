b87c13172ad9dec70f29ed57dd6e01876c4f9a4d
wangze <wangze0218@qq.com>
2017-09-04 08:46:57 +0000

    1

--- resources/views/front/service.blade.php
+++ resources/views/front/service.blade.php
@@ -100,30 +100,6 @@
                         <p>国内的零售产业将迎来新一轮爆发式的增长，在金融服务业开放政策和云计算、大数据技术双重推动下，互联网金融领域的跨界融合正在成为一种趋势。 在这种背景下，国美金控投资服务有限公司（以下简称国美金控）依托现有产业优势，快速地进行战略布局，打造适用于金融发展与投资业务的大数据战略管控平</p>
                     </div>
                 </div>
-                <div class="media col-md-12 shadow">
-                    <div class="media-left">
-                        <a href="#">
-                            <img class="media-object" data-src="holder.js/100X100" alt="64x64" src="https://www.egger.com/medias/1-2PI-AP-REF-2017-07-Freilager-Zuerich-CH-01-250x196.jpg?context=bWFzdGVyfHJvb3R8Mjk3NDl8aW1hZ2UvanBlZ3xoZTIvaDNjL2gwMC84OTY5ODc0NDcyOTkwLmpwZ3w2MTc2N2NkYTg2MjgzYTE2ZDVjNTc5YzE3M2NiZmQ0MWZlNGVlNTc2ODBlNDI1ZjRjYjk5OTlhM2UwMGQxY2Ji&attachment=true"
-                                 data-holder-rendered="true" style="width: 270px; height: 180px;">
-                        </a>
-                    </div>
-                    <div class="media-body">
-                        <h5 class="media-heading">国美金控——让消费金融可视更可靠</h5>
-                        <p>国内的零售产业将迎来新一轮爆发式的增长，在金融服务业开放政策和云计算、大数据技术双重推动下，互联网金融领域的跨界融合正在成为一种趋势。 在这种背景下，国美金控投资服务有限公司（以下简称国美金控）依托现有产业优势，快速地进行战略布局，打造适用于金融发展与投资业务的大数据战略管控平</p>
-                    </div>
-                </div>
-                <div class="media col-md-12 shadow">
-                    <div class="media-left">
-                        <a href="#">
-                            <img class="media-object" data-src="holder.js/100X100" alt="64x64" src="https://www.egger.com/medias/1-2PI-AP-REF-2017-07-Freilager-Zuerich-CH-01-250x196.jpg?context=bWFzdGVyfHJvb3R8Mjk3NDl8aW1hZ2UvanBlZ3xoZTIvaDNjL2gwMC84OTY5ODc0NDcyOTkwLmpwZ3w2MTc2N2NkYTg2MjgzYTE2ZDVjNTc5YzE3M2NiZmQ0MWZlNGVlNTc2ODBlNDI1ZjRjYjk5OTlhM2UwMGQxY2Ji&attachment=true"
-                                 data-holder-rendered="true" style="width: 270px; height: 180px;">
-                        </a>
-                    </div>
-                    <div class="media-body">
-                        <h5 class="media-heading">国美金控——让消费金融可视更可靠</h5>
-                        <p>国内的零售产业将迎来新一轮爆发式的增长，在金融服务业开放政策和云计算、大数据技术双重推动下，互联网金融领域的跨界融合正在成为一种趋势。 在这种背景下，国美金控投资服务有限公司（以下简称国美金控）依托现有产业优势，快速地进行战略布局，打造适用于金融发展与投资业务的大数据战略管控平</p>
-                    </div>
-                </div>
             </div>
             <div class="row">
                 <div class="bs-example" data-example-id="simple-pager">
