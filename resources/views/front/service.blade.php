c0f1a496d1b61e608da00b768a2e574bd752a170
wangze <wangze0218@qq.com>
2017-09-05 10:57:08 +0000

    服务

--- resources/views/front/service.blade.php
+++ resources/views/front/service.blade.php
@@ -1,47 +1,41 @@
 @extends('front.public.master')
 @section('script')
     <style>
-        body{
-            /*background-color: #f6f6f6;*/
-        }
         .page-title{
-            margin: 40px 0px;
-        }
-        .media:first-child{
-            margin-top: 20px;
-
-        }
-        .media{
-            margin-top: 20px;
-            overflow: hidden;
-        }
-        .media-body{
-            padding: 20px 20px;
-        }
-        /*.media-body a{*/
-            /*font-size: 20px;*/
-            /*line-height: 1.4;*/
-            /*color: #e31937;*/
-            /*text-decoration: none;*/
-            /*outline: none;*/
-        /*}*/
-        h5{
-            font-size: 1.6rem;
-            font-weight: bold;
-            height: 24px;
-            line-height: 24px;
-            overflow: hidden;
-            margin: 16px 0;
-        }
-        p{
-            color: #999;
-            font-size: 1.2rem;
-            line-height: 20px;
-            max-height: 120px;
-            overflow: hidden;
-        }
-        .bs-example{
             margin: 50px 0px;
+        }
+        .thumbnail{
+            border: 0;
+        }
+        .thumbnail p:nth-child(1){
+            text-decoration: none;
+            color: #333333;
+            font-size: 26px;
+            text-align: center;
+            margin-bottom: 25px;
+            letter-spacing: 1px;
+            font-weight: bold;
+            padding-top:20px ;
+            padding-bottom:0px ;
+        }
+        .thumbnail p:nth-child(2){
+            display: block;
+            height: 5px;
+            background: url(http://www.chinadata8.com/public/img/yqfw-img01.png) bottom center no-repeat;
+            text-align: center;
+            font-size: 26px;
+            text-align: center;
+            margin-bottom: 25px;
+            margin-top: -15px;
+            letter-spacing: 1px;
+        }
+        .thumbnail p:nth-child(3){
+            line-height: 30px;
+            font-size: 16px;
+            height: 123px;
+            overflow: hidden;
+            text-indent: 2em;
+            padding: 0px 10px;
         }
     </style>
 @endsection
@@ -51,64 +45,102 @@
             <div class="page-title">
                 <h1>产品服务中心</h1>
             </div>
-            <div class="row">
-                <div class="media col-md-12 shadow">
-                    <div class="media-left">
-                        <a href="#">
-                            <img class="media-object" data-src="holder.js/100X100" alt="64x64" src="https://www.egger.com/medias/1-2PI-AP-REF-2017-07-Freilager-Zuerich-CH-01-250x196.jpg?context=bWFzdGVyfHJvb3R8Mjk3NDl8aW1hZ2UvanBlZ3xoZTIvaDNjL2gwMC84OTY5ODc0NDcyOTkwLmpwZ3w2MTc2N2NkYTg2MjgzYTE2ZDVjNTc5YzE3M2NiZmQ0MWZlNGVlNTc2ODBlNDI1ZjRjYjk5OTlhM2UwMGQxY2Ji&attachment=true"
-                                 data-holder-rendered="true" style="width: 270px; height: 180px;">
-                        </a>
-                    </div>
-                    <div class="media-body">
-                        <h5 class="media-heading">国美金控——让消费金融可视更可靠</h5>
-                        <p>国内的零售产业将迎来新一轮爆发式的增长，在金融服务业开放政策和云计算、大数据技术双重推动下，互联网金融领域的跨界融合正在成为一种趋势。 在这种背景下，国美金控投资服务有限公司（以下简称国美金控）依托现有产业优势，快速地进行战略布局，打造适用于金融发展与投资业务的大数据战略管控平</p>
-                    </div>
-                </div>
-                <div class="media col-md-12 shadow">
-                    <div class="media-left">
-                        <a href="#">
-                            <img class="media-object" data-src="holder.js/100X100" alt="64x64" src="https://www.egger.com/medias/1-2PI-AP-REF-2017-07-Freilager-Zuerich-CH-01-250x196.jpg?context=bWFzdGVyfHJvb3R8Mjk3NDl8aW1hZ2UvanBlZ3xoZTIvaDNjL2gwMC84OTY5ODc0NDcyOTkwLmpwZ3w2MTc2N2NkYTg2MjgzYTE2ZDVjNTc5YzE3M2NiZmQ0MWZlNGVlNTc2ODBlNDI1ZjRjYjk5OTlhM2UwMGQxY2Ji&attachment=true"
-                                 data-holder-rendered="true" style="width: 270px; height: 180px;">
-                        </a>
-                    </div>
-                    <div class="media-body">
-                        <h5 class="media-heading">国美金控——让消费金融可视更可靠</h5>
-                        <p>国内的零售产业将迎来新一轮爆发式的增长，在金融服务业开放政策和云计算、大数据技术双重推动下，互联网金融领域的跨界融合正在成为一种趋势。 在这种背景下，国美金控投资服务有限公司（以下简称国美金控）依托现有产业优势，快速地进行战略布局，打造适用于金融发展与投资业务的大数据战略管控平</p>
+            <div class="row" style="width: 1080px">
+                <div class="col-md-4">
+                    <div class="thumbnail">
+                        <img data-src="holder.js/100%x200" alt="100%x200" src="http://www.chinadata8.com/public/img/yqfw-img01.jpg" data-holder-rendered="true" style="height: 320px; width: 320px; display: block;">
+                        <div class="caption">
+                            <p>境外社交媒体舆情分析</p>
+                            <p></p>
+                            <p>采用我司研制的网络智能数据采集技术，按照客户的需求，对其在社交媒体上出现的相关信息...
+                            </p>
+
+                            <p class="text-center">
+                                <button type="button" class="btn-lg btn btn-default" style="width: 80%">了解更多</button>
+                            </p>
+                        </div>
                     </div>
                 </div>
-                <div class="media col-md-12 shadow">
-                    <div class="media-left">
-                        <a href="#">
-                            <img class="media-object" data-src="holder.js/100X100" alt="64x64" src="https://www.egger.com/medias/1-2PI-AP-REF-2017-07-Freilager-Zuerich-CH-01-250x196.jpg?context=bWFzdGVyfHJvb3R8Mjk3NDl8aW1hZ2UvanBlZ3xoZTIvaDNjL2gwMC84OTY5ODc0NDcyOTkwLmpwZ3w2MTc2N2NkYTg2MjgzYTE2ZDVjNTc5YzE3M2NiZmQ0MWZlNGVlNTc2ODBlNDI1ZjRjYjk5OTlhM2UwMGQxY2Ji&attachment=true"
-                                 data-holder-rendered="true" style="width: 270px; height: 180px;">
-                        </a>
-                    </div>
-                    <div class="media-body">
-                        <h5 class="media-heading">国美金控——让消费金融可视更可靠</h5>
-                        <p>国内的零售产业将迎来新一轮爆发式的增长，在金融服务业开放政策和云计算、大数据技术双重推动下，互联网金融领域的跨界融合正在成为一种趋势。 在这种背景下，国美金控投资服务有限公司（以下简称国美金控）依托现有产业优势，快速地进行战略布局，打造适用于金融发展与投资业务的大数据战略管控平</p>
-                    </div>
-                </div>
-                <div class="media col-md-12 shadow">
-                    <div class="media-left">
-                        <a href="#">
-                            <img class="media-object" data-src="holder.js/100X100" alt="64x64" src="https://www.egger.com/medias/1-2PI-AP-REF-2017-07-Freilager-Zuerich-CH-01-250x196.jpg?context=bWFzdGVyfHJvb3R8Mjk3NDl8aW1hZ2UvanBlZ3xoZTIvaDNjL2gwMC84OTY5ODc0NDcyOTkwLmpwZ3w2MTc2N2NkYTg2MjgzYTE2ZDVjNTc5YzE3M2NiZmQ0MWZlNGVlNTc2ODBlNDI1ZjRjYjk5OTlhM2UwMGQxY2Ji&attachment=true"
-                                 data-holder-rendered="true" style="width: 270px; height: 180px;">
-                        </a>
-                    </div>
-                    <div class="media-body">
-                        <h5 class="media-heading">国美金控——让消费金融可视更可靠</h5>
-                        <p>国内的零售产业将迎来新一轮爆发式的增长，在金融服务业开放政策和云计算、大数据技术双重推动下，互联网金融领域的跨界融合正在成为一种趋势。 在这种背景下，国美金控投资服务有限公司（以下简称国美金控）依托现有产业优势，快速地进行战略布局，打造适用于金融发展与投资业务的大数据战略管控平</p>
+                <div class="col-md-4">
+                    <div class="thumbnail">
+                        <img data-src="holder.js/100%x200" alt="100%x200" src="http://www.chinadata8.com/public/img/yqfw-img02.jpg" data-holder-rendered="true" style="height: 320px; width: 320px; display: block;">
+                        <div class="caption">
+                            <p>媒体舆情监测</p>
+                            <p></p>
+                            <p>采用我司研制的网络智能数据采集技术，按照客户的需求，对其在网络媒体上出现的相关信息...
+                            </p>
+                            <p class="text-center">
+                                <button type="button" class="btn-lg btn btn-default" style="width: 80%">了解更多</button>
+                            </p>
+
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="row">
-                <div class="bs-example" data-example-id="simple-pager">
-                    <nav aria-label="..." class="page">
-                        <ul class="pager">
-                            <li><a href="#">上一页</a></li>
-                            <li><a href="#">下一页</a></li>
-                        </ul>
-                    </nav>
+                <div class="col-md-4">
+                    <div class="thumbnail">
+                        <img data-src="holder.js/100%x200" alt="100%x200" src="http://www.chinadata8.com/public/img/yqfw-img03.jpg" data-holder-rendered="true" style="height: 320px; width: 320px; display: block;">
+                        <div class="caption">
+                            <p>全网舆情监测</p>
+                            <p></p>
+                            <p>采用我司研制的网络智能数据采集技术，通过对互联网海量信息进行自动抓取、分类聚类...
+                            </p>
+                            <p class="text-center">
+                                <button type="button" class="btn-lg btn btn-default" style="width: 80%">了解更多</button>
+                            </p>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="thumbnail">
+                        <img data-src="holder.js/100%x200" alt="100%x200" src="http://www.chinadata8.com/public/img/yqfw-img04.jpg" data-holder-rendered="true" style="height: 320px; width: 320px; display: block;">
+                        <div class="caption">
+                            <p>危机应对顾问</p>
+                            <p></p>
+                            <p>采用我司研制的网络智能数据采集技术，为客户及其在网络中出现的相关信息进行实时采集...
+                            </p>
+                            <p class="text-center">
+                                <button type="button" class="btn-lg btn btn-default" style="width: 80%">了解更多</button>
+                            </p>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="thumbnail">
+                        <img data-src="holder.js/100%x200" alt="100%x200" src="http://www.chinadata8.com/public/img/yqfw-img05.jpg" data-holder-rendered="true" style="height: 320px; width: 320px; display: block;">
+                        <div class="caption">
+                            <p>按需舆情报告
+
+                            </p>
+                            <p></p>
+                            <p>按照客户的需求，对其提供多地域、多媒介的舆情采集与分析服务，生成多种形态的分析结果...
+
+                            </p>
+                            <p class="text-center">
+                                <button type="button" class="btn-lg btn btn-default" style="width: 80%">了解更多</button>
+                            </p>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="thumbnail">
+                        <img data-src="holder.js/100%x200" alt="100%x200" src="http://www.chinadata8.com/public/img/yqfw-img06.jpg" data-holder-rendered="true" style="height: 320px; width: 320px; display: block;">
+                        <div class="caption">
+                            <p>境外社交媒体舆情
+
+                            </p>
+                            <p></p>
+                            <p>提供境外社交媒体（如Facebook、Twitter、Line、YouTube等）舆情服务，包括信息采集...
+
+                            </p>
+                            <p class="text-center">
+                                <button type="button" class="btn-lg btn btn-default" style="width: 80%">了解更多</button>
+                            </p>
+
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
