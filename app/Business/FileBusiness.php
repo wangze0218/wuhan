f6a36703ffb6a36c551224ab2a2fb2e09278c007
wangze <wangze0218@qq.com>
2017-09-07 10:37:55 +0000

    文件

---
+++ app/Business/FileBusiness.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wangze
+ * Date: 2017/8/29
+ * Time: 14:53
+ */
+
+namespace App\Business;
+
+use App\Model\ArticleModel;
+use App\System\ResponseException;
+
+class ArticleBusiness
+{
+    
+}
