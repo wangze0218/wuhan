bbd32aa1cad46d1502946f439d47eba81d685c93
wangze <wangze0218@qq.com>
2018-03-26 13:54:27 +0800

    sign

--- resources/views/front/sign.blade.php
+++ resources/views/front/sign.blade.php
@@ -76,7 +76,7 @@
         <div class="container">
                 <div class="row sing_bg text-center">
                     <a href="#">
-                        <img src="/img/huayunshuhai.png" alt="logo">
+                        <img style="width: 100%;" src="/img/huayunshuhai.png" alt="logo">
                     </a>
                 </div>
                 <div class="row">
