52ad850f77d28c3bc81d262cfb16bf284595e778
wangze <wangze0218@qq.com>
2018-03-28 14:10:30 +0800

    jiejuefangan

--- resources/views/front/solution.blade.php
+++ resources/views/front/solution.blade.php
@@ -5,66 +5,87 @@
             margin-top: 140px;
         }
         /*.page-title{*/
-            /*margin: 40px 0px;*/
+            /*margin: 50px 0px;*/
         /*}*/
-        .media:first-child{
-            margin: 30px 0px;
-
-        }
-        .media{
-            margin: 30px 0px;
-            background-color: #fff;
-            border: 1px solid #fff;
-        }
-        .media-body{
-            padding: 20px 20px;
-        }
-        .media-body h3{
-            display: block;
-            font-size: 16px;
-            margin: 0 0 6px;
-            color: #8b8d8e;
-        }
-        .media-body .p1{
-            font-size: 22px;
-            margin-bottom: 8px;
-        }
-        .media-body .p2{
-            width:70%;
-            font-size: 12px;
-            margin-bottom: 7px;
-            overflow-wrap: break-word;
-            word-wrap: break-word;
-            -webkit-hyphens: auto;
-            -ms-hyphens: auto;
-            -moz-hyphens: auto;
-            hyphens: auto;
-            line-height: 1.375;
-        }
-        .media-body a{
-            font-size: 20px;
-            line-height: 1.4;
-            color: #e31937;
-            text-decoration: none;
-            outline: none;
-        }
-        .list-group-item{
-            border: 0px;
-        }
         .thumbnail{
-            border: 0px;
-        }
-        .bs-example{
-            margin: 50px 0px;
+            border: 0;
         }
         @media (min-width: 768px){
-
-        }
-        @media screen and (max-width: 768px){
-            .shadow{
-                box-shadow: 0 0px 0px 0px rgba(0, 0, 0, .6)!important;
+            .thumbnail img{
+                height: 320px;
+                width: 320px;
+                display: block;
+            }
+            .thumbnail p:nth-child(1){
+                text-decoration: none;
+                color: #333333;
+                font-size: 26px;
+                text-align: center;
+                margin-bottom: 25px;
+                letter-spacing: 1px;
+                font-weight: bold;
+                padding-top:20px ;
+                padding-bottom:0px ;
+            }
+            .thumbnail p:nth-child(2){
+                display: block;
+                height: 5px;
+                background: url(http://www.chinadata8.com/public/img/yqfw-img01.jpg) bottom center no-repeat;
+                text-align: center;
+                font-size: 26px;
+                text-align: center;
+                margin-bottom: 25px;
+                margin-top: -15px;
+                letter-spacing: 1px;
+            }
+            .thumbnail p:nth-child(3){
+                line-height: 30px;
+                font-size: 16px;
+                height: 123px;
+                overflow: hidden;
+                text-indent: 2em;
+                padding: 0px 10px;
             }
         }
+        @
+        media screen and (max-width: 768px){
+            .thumbnail img{
+                width: 100%;
+                display: block;
+            }
+            .thumbnail p:nth-child(1){
+                height: 0px;
+                text-decoration: none;
+                color: #333333;
+                font-size: 18px;
+                text-align: center;
+                margin-bottom: 25px;
+                letter-spacing: 1px;
+                font-weight: bold;
+                padding-top:20px ;
+                padding-bottom:0px ;
+            }
+            .thumbnail p:nth-child(2){
+                display: block;
+                height: 5px;
+                background: url(http://www.chinadata8.com/public/img/yqfw-img01.jpg) bottom center no-repeat;
+                text-align: center;
+                font-size: 26px;
+                text-align: center;
+                margin-bottom: 25px;
+                margin-top: -15px;
+                letter-spacing: 1px;
+            }
+            .thumbnail p:nth-child(3){
+                line-height: 30px;
+                font-size: 16px;
+                height: 123px;
+                overflow: hidden;
+                text-indent: 2em;
+                padding: 0px 10px;
+            }
+        }
+
 
     </style>
 @endsection
@@ -72,27 +93,32 @@
     <div class="container-fluid">
         <div class="container">
             {{--<div class="page-title">--}}
-                {{--<h1>解决方案</h1>--}}
+                {{--<h2>&nbsp</h2>--}}
             {{--</div>--}}
-            <div class="row">
-                <div class="col-md-12 shadow">
-                    {{--<div class="group-list" style="padding: 0px 30px;background-color: #ddd">--}}
-                        {{--<h2 style="border-bottom: solid 1px #d7d7d7;padding: 30px 0px">华云数海（大数据）核心业务</h2>--}}
-                    {{--</div>--}}
-                    @foreach($articles as $k=>$v)
-                        <div class="media col-md-6 ">
-                            <a href="/details/{{$v['article_id']}}" style="color: #333">
-                                <div class="media-body">
-                                    <p class="p1">{{$k+1}}.{{$v['title']}}</p>
-                                    <p class="p2">{{$v['title_describe']}}</p>
-                                </div>
+            <div class="row" style="">
+                @foreach($articles as $k=>$v)
+                    <div class="col-xs-6 col-sm-4">
+                        <div class="thumbnail">
+                            <a href="/details/{{$v['article_id']}}">
+                                <img data-src="holder.js/100%x200" alt="100%x200" src="{{$v['title_img']}}" data-holder-rendered="true">
                             </a>
+                            <div class="caption">
+                                <p>{{$v['title']}}</p>
+                                <p></p>
+                                <p>{{$v['title_describe']}}</p>
+
+                                <p class="text-center">
+                                    <a href="/details/{{$v['article_id']}}">
+                                    <button type="button" class="btn-lg btn btn-default" style="width: 80%">了解更多</button>
+                                    </a>
+                                </p>
+                            </div>
                         </div>
-                    @endforeach
-                </div>
+                    </div>
+                @endforeach
+
 
             </div>
-            {!! $page_view !!}
         </div>
     </div>
 @endsection
