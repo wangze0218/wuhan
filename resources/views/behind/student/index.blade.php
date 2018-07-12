8571256fc83f8aa7e52e90ddeb03d1b5b7dcfb0b
wangze <418890061@qq.com>
2018-07-12 22:24:51 +0800

    后台学生管理 列表修改

--- resources/views/behind/student/index.blade.php
+++ resources/views/behind/student/index.blade.php
@@ -17,6 +17,8 @@
                     <th><i class="icon-bullhorn"></i> 序号</th>
                     <th class="hidden-phone"><i class="icon-question-sign"></i> 学生姓名</th>
                     <th><i class=" icon-tasks"></i> 学生身份证号</th>
+                    <th><i class=" icon-tasks"></i> 专业</th>
+                    <th><i class=" icon-tasks"></i> 方向</th>
                     <th><i class="icon-bookmark"></i> 操作</th>
                 </tr>
                 </thead>
@@ -27,6 +29,8 @@
                                 <td><a href="#">{{$k+1}}</a></td>
                                 <td>{{$v->student_name}}</td>
                                 <td>{{$v->identity}}</td>
+                                <td>{{$v->major}}</td>
+                                <td>{{$v->direction}}</td>
                                 <td>
                                     <button onclick="return addOrEdit('/behind/student/{{$v->student_id}}/edit','学生编辑',['1000px','800px']);"
                                             class="btn btn-primary btn-xs">编辑</button>
